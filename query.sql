371482b28ac7c8f719ad4d460b521e4582746b88
Abdurashid011 <abdumajidovabdurashid1504@gmail.com>
2024-07-26 03:46:37 +0500

    Changed to sql query

--- query.sql
+++ query.sql
@@ -2,16 +2,17 @@ CREATE DATABASE todo_list;
 
 USE todo_list;
 
-CREATE TABLE todos (
-                       id INT AUTO_INCREMENT PRIMARY KEY,
-                       title VARCHAR(255) NOT NULL,
-                       completed BOOLEAN NOT NULL DEFAULT 0
+CREATE TABLE users (
+
+           id INT AUTO_INCREMENT PRIMARY KEY,
+           chat_id BIGINT,
+           status VARCHAR(32);
 );
 
-CREATE TABLE users (
-                                       id INT AUTO_INCREMENT PRIMARY KEY,
-                                       `add` VARCHAR(255),
-                                       `check` VARCHAR(255),
-                                       `uncheck` VARCHAR(255),
-                                       `delete` VARCHAR(255)
+CREATE TABLE todos (
+
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            user_id INT,
+            title VARCHAR(255),
+            status tinyint(1)
 );
