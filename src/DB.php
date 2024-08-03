ebe87c7474622826da976b4a35d7f2adbdcf3287
Abdurashid011 <abdumajidovabdurashid1504@gmail.com>
2024-08-04 00:41:42 +0500

    PATCH: Changed host, db_name, username and password to ENV to connect to database.

--- src/DB.php
+++ src/DB.php
@@ -6,7 +6,10 @@ class DB
 {
     public static function connect(): PDO
     {
-        $pdo = "{$_ENV['DB_CONNECTION']}:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_NAME']};user={$_ENV['DB_USERNAME']};password={$_ENV['DB_PASSWORD']}";
+        $pdo = "{$_ENV['DB_CONNECTION']}:host={$_ENV['DB_HOST']};
+        dbname={$_ENV['DB_NAME']};
+        user={$_ENV['DB_USERNAME']};
+        password={$_ENV['DB_PASSWORD']}";
         return new PDO($pdo);
     }
 }
