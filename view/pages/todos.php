08fa7b94ab22f0d849cedf4519725a4c5a4a894c
Abdurashid011 <abdumajidovabdurashid1504@gmail.com>
2024-08-06 16:44:51 +0500

    PATCH navbar has been fixed

--- view/pages/todos.php
+++ view/pages/todos.php
@@ -9,7 +9,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/c4497f215d.js" crossorigin="anonymous"></script>
+    <title>TODO App</title>
     <title>To-do List</title>
     <style>
         .status {
